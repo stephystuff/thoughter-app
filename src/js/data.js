d73a4888d0b2ca91d58433e3aa802f833b116822
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 14:26:08 -0500

    Error: Function to create a new thought should go here. Function to submit form goes in ui.

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,26 @@
 (function() {
   'use strict';
 
-  console.log("hey there!");
+  window.thoughter = window.thoughter || {};
+
+  $('.new-post')
+      .on('submit', function submitThought(event) {
+        event.preventDefault();
+        console.log('this works');
+         $.ajax({
+            url: 'https://thoughter.herokuapp.com/api/Thoughts',
+            method: 'POST',
+            data: JSON.stringify({'content':newPost}),
+            headers: {'Content-Type': 'application/json'}
+      })
+      .done(function successHandler(data) {
+        $('.panel-info');
+        console.log('hey', data);
+      })
+      .fail(function failHandler(data) {
+        console.log('try again!', data);
+      });
+    });
 
 
 
