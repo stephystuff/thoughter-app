e453d84db89149df9e945dd05ac4cb75e29fb271
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 15:17:04 -0500

    Continued work on function to create new thought. Received error in console for data. Stringify was mispelled. Fixing typo and trying again.

--- build/js/app.js
+++ build/js/app.js
@@ -34,3 +34,26 @@
 
 
 }());
+
+(function() {
+  'use strict';
+
+  window.thoughter = window.thoughter || {};
+
+  $('main').hide();
+    window.addEventListener('hashchange', function() {
+      $('main').hide();
+        console.log("I'm in here!");
+      $(window.location.hash).show();
+  });
+
+  $('.post-new-thought')
+      .on('submit', function postThought (event) {
+        event.preventDefault();
+        window.thoughter.createThought($('.form-control').val());
+      });
+
+
+
+
+}());
