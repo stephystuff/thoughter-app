1734c25e4e5d89afe1293d0cc2b1679161429621
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 14:24:27 -0500

    began writing function to create a new thought.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,11 +1,17 @@
 (function() {
   'use strict';
 
-console.log('Hello, world!');
+  window.thoughter = window.thoughter || {};
+  window.thoughter.newPost = function newPost(thought) {
 
 
+  $('main').hide();
 
-
+    window.addEventListener('hashchange', function() {
+    $('main').hide();
+    console.log("I'm in here!");
+    $(window.location.hash).show();
+  });
 
 
 
