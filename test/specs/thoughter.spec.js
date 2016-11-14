8c37ab7dda8d9737e3bf7573468521badfe28321
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 21:49:13 -0500

    Test for namespace working.

---
+++ test/specs/thoughter.spec.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  var expect = chai.expect;
+
+    describe('Data Module', function(){
+      it('should expect namespace to exist');
+        expect(window.thoughter).to.be.an('object');
+    });
+
+}());
