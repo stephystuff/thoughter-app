b101657109fc172eab0bdf2a818b0584bd43c162
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 20:25:31 -0500

    Fixed typo in class name. Hopefully able to append to ul now.

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,13 +2,14 @@
   'use strict';
 
   window.thoughter = window.thoughter || {};
+
   $('main').hide();
     window.addEventListener('hashchange', function() {
       $('main').hide();
         console.log("I'm in here!");
       $(window.location.hash).show();
 
-      if(window.location.hash === '#recent-thought'){
+      if(window.location.hash === '#recent-thoughts'){
         window.thoughter.retrieveAllThoughts()
         .done(function successHandler(data){
           console.log(data, 'nav success');
