2913f3da0ba474997dfb62a63cd94bc0e62b2087
StephyStuff <stephanie.birch13@gmail.com>
2016-11-13 20:24:02 -0500

    Still trying to append to recent thoughts.

--- build/js/app.js
+++ build/js/app.js
@@ -2,7 +2,21 @@
   'use strict';
 
   window.thoughter = window.thoughter || {};
+
+  $('.post-new-thought')
+      .on('submit', function postThought (event) {
+        event.preventDefault();
+        console.log('submit works');
+        window.thoughter.createThought($('.form-control').val());
+      });
+}());
+
+(function() {
+  'use strict';
+
+  window.thoughter = window.thoughter || {};
   window.thoughter.createThought = createThought;
+  window.thoughter.retrieveAllThoughts = retrieveAllThoughts;
 
     function createThought(newThought) {
       $.ajax({
@@ -22,6 +36,19 @@
       });
     }
 
+    function retrieveAllThoughts(){
+      return $.ajax({
+          url:'https://thoughter.herokuapp.com/api/Thoughts?filter={"order":"createTime DESC"}',
+          method:'GET',
+          dataType: 'json'
+      })
+      .done(function successHandler(data){
+        console.log(data);
+      })
+      .fail(function failHandler(xhr){
+        console.log(xhr, 'attempt failed');
+      });
+    }
 
 
 
@@ -45,16 +72,31 @@
       $('main').hide();
         console.log("I'm in here!");
       $(window.location.hash).show();
+
+      if(window.location.hash === '#recent-thoughts'){
+        window.thoughter.retrieveAllThoughts()
+        .done(function successHandler(data){
+          console.log(data, 'nav success');
+          window.thoughter.addThoughtsToRecent(data);
+        });
+      }
   });
 
-  $('.post-new-thought')
-      .on('submit', function postThought (event) {
-        event.preventDefault();
-        console.log('submit works');
-        window.thoughter.createThought($('.form-control').val());
-      });
 
+}());
 
+(function() {
+  'use strict';
 
+  window.thoughter = window.thoughter || {};
+  window.thoughter.addThoughtsToRecent = addThoughtsToRecent;
+
+  function addThoughtsToRecent(thoughts) {
+    thoughts.forEach(function appendThought(thought){
+      $('.thoughts-list')
+          .append('<li>' + thought.content + '</li>');
+          // .append('<p class=".recent-thought">' + thought.content + '</p>');
+    });
+  }
 
 }());
--- src/js/data.js
+++ src/js/data.js
@@ -3,6 +3,7 @@
 
   window.thoughter = window.thoughter || {};
   window.thoughter.createThought = createThought;
+  window.thoughter.retrieveAllThoughts = retrieveAllThoughts;
 
     function createThought(newThought) {
       $.ajax({
@@ -22,6 +23,19 @@
       });
     }
 
+    function retrieveAllThoughts(){
+      return $.ajax({
+          url:'https://thoughter.herokuapp.com/api/Thoughts?filter={"order":"createTime DESC"}',
+          method:'GET',
+          dataType: 'json'
+      })
+      .done(function successHandler(data){
+        console.log(data);
+      })
+      .fail(function failHandler(xhr){
+        console.log(xhr, 'attempt failed');
+      });
+    }
 
 
 
